217f70ab91e8e80fd032d9d7f23bbf87f8811bc3
Mohd Faizan Moosa <fxizxnmoosx@gmail.com>
2023-08-08 21:02:20 +0530

    Update

--- app/src/main/java/com/faizan/printsecure/QrScanner.java
+++ app/src/main/java/com/faizan/printsecure/QrScanner.java
@@ -1,21 +1,67 @@
 package com.faizan.printsecure;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.widget.TextView;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class QrScanner extends AppCompatActivity {
 
-    private TextView textView;
-    private String text;
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+    private CameraSource cameraSource;
+    private static final int REQUEST_CAMERA_PERMISSION = 201;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qr_scanner);
-        textView = findViewById(R.id.testView);
-        text = getIntent().getStringExtra("downloads");
-        textView.setText(text);
+        surfaceView = findViewById(R.id.surfaceView);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.ALL_FORMATS)
+                .build();
+        cameraSource = new CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(1920, 1080)
+                .setAutoFocusEnabled(true)
+                .build();
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try {
+                    if (ActivityCompat.checkSelfPermission(QrScanner.this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+                        cameraSource.start(surfaceView.getHolder());
+                    }
+                    else {
+                        ActivityCompat.requestPermissions(QrScanner.this, new String[] {
+                                android.Manifest.permission.CAMERA
+                        }, REQUEST_CAMERA_PERMISSION);
+                    }
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
     }
 }
