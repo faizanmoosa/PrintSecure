ee5cfdaf78b163f1a325885aa6d91e45cf29dbf4
Mohd Faizan Moosa <fxizxnmoosx@gmail.com>
2023-08-08 17:35:46 +0530

    Update

--- app/src/main/java/com/faizan/printsecure/VendorCustomer.java
+++ app/src/main/java/com/faizan/printsecure/VendorCustomer.java
@@ -9,15 +9,10 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -27,7 +22,6 @@ import java.io.File;
 public class VendorCustomer extends AppCompatActivity {
     private Button vendor, customer;
     private StorageReference storageReference;
-    private DatabaseReference databaseReference;
     private String downloadURLs = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
